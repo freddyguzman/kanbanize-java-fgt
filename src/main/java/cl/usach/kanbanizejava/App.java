7141c031b29819d8093769e31b5d53a93afbc8ee
freddy.guzman <freddy.guzman@usach.cl>
2014-03-10 19:23:06 -0300

    Merge commit '554067366c7a3f1a76d5ad85ae47094b0ba5eb9e'
    Conflicts:
    	src/main/java/cl/usach/gettrello/MainClass.java
    
    Comit Merge FGT Main

---
+++ src/main/java/cl/usach/kanbanizejava/App.java
@@ -0,0 +1,15 @@
+package cl.usach.kanbanizejava;
+
+import org.json.JSONArray;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
